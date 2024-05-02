59db1583a0156b6c62d3c0c0b325c3f2b5e867ec
ppdx999 <ppdx999@gmail.com>
2024-05-02 13:09:40 +0900

    implement wildcard handler and add test

--- app/main.go
+++ app/main.go
@@ -6,10 +6,22 @@ import (
 )
 
 func main() {
-  http.HandleFunc("/", hander)
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":8080", newRouter())
+}
+
+func newRouter() *http.ServeMux {
+  mux := http.NewServeMux()
+
+  mux.HandleFunc("/", hander)
+  mux.HandleFunc("GET /wildcard/{v}", WildcardHandler)
+
+  return mux
 }
 
 func hander(w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, "Hello World")
+}
+
+func WildcardHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, r.PathValue("v"))
 }
